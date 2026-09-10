a96e42fe3b900b8494272567aed5dc3d8fa04c9f
pbs-tech <alex_peebles@outlook.com>
2026-09-10 22:49:59 +0100

    fixing and updating

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jsdom does not implement matchMedia, and react-hot-toast calls it to check
+// prefers-reduced-motion as soon as a toast renders.
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
